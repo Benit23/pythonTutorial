4063d178e6ae4ea4f2ffdd73d91adf84200b0ed7
Benit23 <bertrandndizeye@gmail.com>
2023-07-11 17:29:24 +0300

    Create User_input.py

---
+++ Day 2/User_input.py
@@ -0,0 +1,13 @@
+# Working wth User input
+
+age = input("Enter your age: ")
+Name = input("Enter your Name: ")
+Address = input("Enter your Address: ")
+School = input("Enter your school: ")
+Number_of_children = input("Enter the number of children: ")
+
+print("There was once a time")
+print("A man called " + Name)
+print("who lived in " + Address)
+print("and schooled at " + School)
+print("He also had " + str(Number_of_children) + " children")

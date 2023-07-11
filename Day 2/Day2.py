fbf70ca2c58ff35884f11e3a71a0430019e09b15
Benit23 <bertrandndizeye@gmail.com>
2023-07-11 16:29:42 +0300

    Day2 first commit

---
+++ Day 2/Day2.py
@@ -0,0 +1,8 @@
+# variables
+
+age = 18
+
+# concatenation: combining 2 expressions (strings) together
+# Typecasting: changing the data type of a variable
+
+print("john is " + str(age) + " years old")

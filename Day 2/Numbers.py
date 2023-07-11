b061a69aea047831baca32e2e8550385f3ef9757
Benit23 <bertrandndizeye@gmail.com>
2023-07-11 19:57:13 +0300

    Addition in python

---
+++ Day 2/Numbers.py
@@ -0,0 +1,10 @@
+# nuMBERS
+
+# Addition
+
+x = input("Enter the value of X : ")
+y = input("Enter the value of y : ")
+
+answer = int(x) + int(y)
+
+print("The result of the Addition is: " + str(answer))

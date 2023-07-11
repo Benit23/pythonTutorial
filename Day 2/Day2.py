c3ac179207033db214daa64de137b1b3d346933e
Benit23 <bertrandndizeye@gmail.com>
2023-07-11 16:48:41 +0300

    First Mini Project

--- Day 2/Day2.py
+++ Day 2/Day2.py
@@ -2,7 +2,15 @@
 
 age = 18
 
+Name = "Bertrand"
+Address = "Magusa"
+School = "CIU"
+Number_of_children = 10
 # concatenation: combining 2 expressions (strings) together
 # Typecasting: changing the data type of a variable
 
-print("john is " + str(age) + " years old")
+print("There was once a time")
+print("A man called " + Name)
+print("who lived in " + Address)
+print("and schooled at " + School)
+print("He also had " + str(Number_of_children) + " children")

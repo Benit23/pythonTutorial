6d4b256b89c4535baca79cbbbea2c6dbb0de9144
Benit23 <bertrandndizeye@gmail.com>
2023-07-11 19:35:38 +0300

    Exercise 1

---
+++ Day 2/Exerciseinput.py
@@ -0,0 +1,13 @@
+# exercise
+
+First_Name = input("Enter your first Name: ")
+Last_Name = input("Enter your last Name: ")
+School = input("Enter your school: ")
+Faculty = input("Enter your Faculty: ")
+Department = input("Enter your Department: ")
+course = input("Enter your favorite course: ")
+
+print("There is a student whose full names are ")
+print(First_Name + " " + Last_Name)
+print("who attends " + School + " in " + Faculty + " Faculty")
+print("and is in " + Department + " department with favorite course as " + course)

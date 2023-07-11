f2f656dc6a67fb265ae0ced2dbfc42c18fe47d68
Benit23 <bertrandndizeye@gmail.com>
2023-07-11 20:14:39 +0300

    The Substraction, Multiplication and Division of variables

--- Day 2/Numbers.py
+++ Day 2/Numbers.py
@@ -8,3 +8,32 @@ y = input("Enter the value of y : ")
 answer = int(x) + int(y)
 
 print("The result of the Addition is: " + str(answer))
+
+
+# Substraction
+
+A = input("Enter the value of A: ")
+B = input("Enter the value of B: ")
+
+Answer = int(A) - int(B)
+
+print("The result of the Substraction is: " + str(Answer))
+
+
+# Multiplication
+
+M = input("Enter the value of M: ")
+N = input("Enter the value of N: ")
+
+Answer = int(M) * int(N)
+
+print("The result if the multiplication is: " + str(Answer))
+
+# Division
+
+E = input("Enter the value of E: ")
+I = input(" Enter the value of I: ")
+
+Answer = int(E) / int(I)
+
+print("The result of the Division is: " + str(Answer))

38f3804da38032bba363c9c29aef0aea27ac3231
Benit23 <bertrandndizeye@gmail.com>
2023-07-14 16:52:43 +0300

    Getting user options

--- Day 3/Temperatureconverter.py
+++ Day 3/Temperatureconverter.py
@@ -8,3 +8,8 @@ print("Menu")
 print(" ")
 print("1) convert from fahrenheit to celcius ")
 print("2) convert from celcius to fahrenheit")
+
+# prompt for user option
+user_option = input("User option: ")
+
+# Perform option check

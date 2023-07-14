4ff483535dc228d9172e225b87693abe748b6f8b
Benit23 <bertrandndizeye@gmail.com>
2023-07-14 17:18:23 +0300

    Option 1

--- Day 3/Temperatureconverter.py
+++ Day 3/Temperatureconverter.py
@@ -13,3 +13,20 @@ print("2) convert from celcius to fahrenheit")
 user_option = input("User option: ")
 
 # Perform option check
+
+if (user_option == 1):
+
+    # formula= 5/9*(f-32)
+    temp_F = input("Enter temperature in fahrenheit")
+
+    # cmputing the tempretarature in f
+    result_F = (5/9) * (int(temp_F) - 32)
+    # Printing the result
+    print("The result is " + str(result_F))
+
+elif (user_option == 2):
+    # formula = (c*(9/5) + 32)
+
+
+else:
+    print("Enter a valid option")

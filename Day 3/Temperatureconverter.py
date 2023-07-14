f96f07d064635ed73af57132d2016917a6eb02c5
Benit23 <bertrandndizeye@gmail.com>
2023-07-14 16:48:51 +0300

    Menu

--- Day 3/Temperatureconverter.py
+++ Day 3/Temperatureconverter.py
@@ -3,3 +3,8 @@
 # Menu section
 
 print("-------- TEMPERATURE CONVERTER ----------")
+print(" ")
+print("Menu")
+print(" ")
+print("1) convert from fahrenheit to celcius ")
+print("2) convert from celcius to fahrenheit")
